2b82a915b832100dd30ea2c66bc33a8d46931e42
yerin <yesyerinno@naver.com>
2019-05-13 20:24:15 +0900

    second commit

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('post/new/', views.post_new, name = 'new'),
     path('post/<int:post_id>/edit/',views.post_edit, name = 'edit'),
     path('post/<int:post_id>/delete/',views.post_delete, name = 'delete'),
+    path('post/<int:post_id>/comment/', views.add_comment, name='add_comment'),
+    path('comment/<int:comment_id>/delete', views.comment_delete, name="comment_delete"),
 ]
